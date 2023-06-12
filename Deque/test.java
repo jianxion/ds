bd7ca1a5b0f35a7a3c04d6d783c4fe79653b0cbe
jianxion <sjxshen@ucdavis.edu>
2023-06-12 00:28:00 -0400

    add abc

---
+++ Deque/test.java
@@ -0,0 +1,10 @@
+package Deque;
+
+public class test {
+        public static void main(String[] args){
+            myDeque<String> mytest = new Deque.ArrayMyDeque<String>();
+            //mytest.addLast("abc");
+            //mytest.printDeque();
+            System.out.println(mytest.isEmpty());
+        }
+}
